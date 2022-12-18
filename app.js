8839395844ebfccf1a754074b581f443023cbf40
truongmi132 <120845861+truongmi132@users.noreply.github.com>
2022-12-17 21:54:31 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var message = 'Hello, World!';
+// create a new heading 1 element
+var heading = document.createElement('h1');
+heading.textContent = message;
+// add the heading the document
+document.body.appendChild(heading);
+console.log(message);
